4ddf6e3f2285d84942227c1703ea6b614aa7e4f1
Egor Kuleshov <Egor.Kuleshov@comindware.ru>
2023-01-09 18:25:05 +0300

    u

---
+++ enc/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace enc
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            foreach (var info in Encoding.GetEncodings())
+            {
+                Console.WriteLine(info.DisplayName);
+            }
+        }
+    }
+}
